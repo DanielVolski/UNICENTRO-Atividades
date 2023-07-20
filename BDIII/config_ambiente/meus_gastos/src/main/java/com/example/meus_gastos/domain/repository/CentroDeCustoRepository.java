20820fcb2ec2f0adb0bf5d5ac86a041ff4b0cc38
Daniel Volski <danfvdaum3@hotmail.com>
2023-07-20 08:20:09 -0300

    Finished CentroDeCustoRepository

---
+++ BDIII/config_ambiente/meus_gastos/src/main/java/com/example/meus_gastos/domain/repository/CentroDeCustoRepository.java
@@ -0,0 +1,12 @@
+package com.example.meus_gastos.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.meus_gastos.domain.model.CentroDeCusto;
+import com.example.meus_gastos.domain.model.Usuario;
+
+public interface CentroDeCustoRepository extends JpaRepository<CentroDeCusto, Long> {
+    List<CentroDeCusto> findByUsuario(Usuario usuario);
+}
