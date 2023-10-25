92557283ac2cccacff61f70609e21afd85eb358e
Daniel Volski <danfvdaum3@hotmail.com>
2023-10-25 20:20:34 -0300

    Finished app-calc

--- Computação Móvel/app-calc/src/app/home/home.page.ts
+++ Computação Móvel/app-calc/src/app/home/home.page.ts
@@ -6,47 +6,110 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  expression: string = '0';
-  value1!: number;
-  value2!: number;
-  operation!: number;
+  display: string = '0';
+  expression: string = '';
 
-  constructor() {}
-
-  showValue(value: string) {
-    if (this.expression.length && this.expression == '0') {
-      this.expression = value;
-    } else {
-      this.expression += value;
+  displayButtonContent(value: string) {
+    if (this.display == '0')
+      this.display = value;
+    else {
+      if (!(value == '.' && this.display.includes('.')))
+        this.display += value;
     }
   }
 
-  addOperation(valor: number) {
-    this.operation = valor;
-    this.value1 = +this.expression;
-    this.value1 = Number(this.expression);
-    this.zero();
+  clearDisplay() {
+    this.display = '0';
   }
 
-  zero() {
-    this.expression = '0';
+  clearExpression() {
+    this.expression = '';
+  }
+
+  handleOperation(op: string) {
+    console.log(this.expression);
+    this.expression += this.display;
+    this.expression = this.expression + op;
+    this.clearDisplay();
+  }
+
+  negate() {
+    if (this.display != '0')
+      this.display = String(-Number(this.display));
+  }
+
+  percentage() {
+    this.display = String(Number(this.display) / 100)
   }
 
   evaluate() {
-    this.value2 = +this.expression;
-    switch (this.operation) {
-      case 0:
-        this.expression = "" + (this.value1 + this.value2);
-        break;
-      case 1:
-        this.expression = "" + (this.value1 - this.value2);
-        break;
-      case 2:
-        this.expression = "" + (this.value1 * this.value2);
-        break;
-      case 3:
-        this.expression = "" + (this.value1 / this.value2);
-        break;
+    let stack: string[] = [];
+    let outputQueue: string[] = [];
+    let expression = this.expression + this.display;
+
+    for (let token of expression.split(/([+\-*/])/)) {
+      token = token.trim();
+
+      if (!token) continue;
+
+      if (!isNaN(Number(token))) {
+        outputQueue.push(token);
+      } else if (this.isOperator(token)) {
+        while (
+          stack.length > 0 &&
+          this.isOperator(stack[stack.length - 1]) &&
+          this.getPrecedence(token) <= this.getPrecedence(stack[stack.length - 1])
+        ) {
+          outputQueue.push(stack.pop()!);
+        }
+        stack.push(token);
+      }
+    }
+
+    while (stack.length > 0)
+      outputQueue.push(stack.pop()!);
+
+    stack = [];
+    for (let token of outputQueue) {
+      if (!this.isOperator(token)) {
+        stack.push(token);
+      } else {
+        let b = parseFloat(stack.pop()!);
+        let a = parseFloat(stack.pop()!);
+        let result = this.performOperation(a, b, token);
+        stack.push(result.toString());
+      }
+    }
+
+    if (stack.length === 1) {
+      this.display = stack[0];
+    } else {
+      this.display = 'Invalid Expression';
+    }
+
+    this.clearExpression();
+  }
+
+  private isOperator(token: string): boolean {
+    return /^[+\-*/]$/.test(token);
+  }
+
+  private getPrecedence(operator: string): number {
+    return (operator == '*' || operator == '/' ? 2 : 1);
+  }
+
+  private performOperation(a: number, b: number, operator: string): number {
+    switch (operator) {
+      case '+':
+        return a + b;
+      case '-':
+        return a - b;
+      case '*':
+        return a * b;
+      case '/':
+        return (b == 0 ? NaN : a / b);
+      default:
+        return NaN;
     }
   }
 }
