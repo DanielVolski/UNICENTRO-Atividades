52bd501734502fd5c24bdb184d89d73518e7839e
Daniel Volski <danfvdaum3@hotmail.com>
2023-10-27 12:58:02 -0300

    Finished movie-details and movies

--- Computação Móvel/app_filmes_ta/src/app/movie-details/movie-details.page.ts
+++ Computação Móvel/app_filmes_ta/src/app/movie-details/movie-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OmdbapiService } from '../services/omdbapi.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
+  info: any;
 
-  constructor() { }
+  constructor(private actroute: ActivatedRoute, private omdbapi: OmdbapiService) { }
 
   ngOnInit() {
+    let id = this.actroute.snapshot.paramMap.get('id');
+    this.omdbapi.getById(id).subscribe(result => this.info = result);
   }
 
+  openURL() {
+    window.open(this.info.Website, '_blank');
+  }
 }
